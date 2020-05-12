be73ec656c473eb9ebe75a3c5d18da8a1159dae8
Christian Lorenzo <clbmiami2004@gmail.com>
2020-05-11 18:59:49 -0700

    Added P.List permission and added functions for permission in code.

--- VideoRecorder/VideoRecorder/CameraViewController.swift
+++ VideoRecorder/VideoRecorder/CameraViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
 
@@ -20,7 +21,43 @@ class CameraViewController: UIViewController {
 		// Resize camera preview to fill the entire screen
 		cameraView.videoPlayerLayer.videoGravity = .resizeAspectFill
 	}
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        requestPermissionAndShowCamera()
+    }
+    
+    private func requestPermissionAndShowCamera() {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .notDetermined:
+            return requestVideoPermission()
+        case .restricted:
+            preconditionFailure("Video is disabled, please review device restrictions")
+        case .denied:
+            preconditionFailure("Tell the user they can't use the app without giving permissions via Settings > Privacy > Video")
+        case .authorized:
+            showCamera()
+        @unknown default:
+            preconditionFailure("A new status code was added that we need to handle")
+        }
+    }
 
+    private func requestVideoPermission() {
+        AVCaptureDevice.requestAccess(for: .video) { (isGranted) in
+            guard isGranted else {
+                preconditionFailure("UI: Tell the user to enable permissions for Video/Camera")
+            }
+            
+            DispatchQueue.main.async {
+                self.showCamera()
+            }
+        }
+    }
+    
+    private func showCamera() {
+        
+    }
 
     @IBAction func recordButtonPressed(_ sender: Any) {
 
